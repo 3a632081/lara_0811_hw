cf09868ff17c72660042eb4c61fe433ca39fa02c
3a632081 <s3a632081@student.ncut.edu.tw>
2019-10-21 19:59:21 +0800

    修改resource/view下的3個php檔內容

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,1 +1,45 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    <style>
+        html, body {
+            height: 100%;
+        }
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato', sans-serif;
+        }
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+        .title {
+            font-size: 96px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">News</div>
+        <input type="button" value="Home" onclick="self.location.href='{{route('home.index')}}'"/>
+        <input type="button" value="About" onclick="self.location.href='{{route('about.index')}}'"/>
+
+
+    </div>
+</div>
+</body>
+</html>
